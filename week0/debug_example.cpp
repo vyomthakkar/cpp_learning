77285362bf9e3eb0840393f73ac8463bb97a2712
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2025-10-24 17:50:00 +0530

    made the for loop, for each syntax

--- week0/debug_example.cpp
+++ week0/debug_example.cpp
@@ -5,8 +5,8 @@ int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5};
     
     int sum = 0;
-    for (int i = 0; i < numbers.size(); i++) {
-        sum += numbers[i];
+    for (int num: numbers) {
+        sum += num;
     }
 
     std::cout << "Sum: " << sum << std::endl;
